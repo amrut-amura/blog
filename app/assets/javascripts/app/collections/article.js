8bf67bdd1a13ebba64e7f48017ee88c4ae027a9f
Amrut <amrut@amuratech.com>
2016-10-26 18:56:41 +0530

    event on model instead of collection

--- app/assets/javascripts/app/collections/article.js
+++ app/assets/javascripts/app/collections/article.js
@@ -2,13 +2,6 @@ App.Collections.article = Backbone.Collection.extend({
 	url: '/articles/',
 	model : App.Models.articlemodel,
 	initialize:function(options){
-		template: JST["app/templates/article_list"],
-		this.on("change",function (argument) {
-			// debugger;
-				layout.getRegion('article_list').show(new App.Views.articleslistview({
-				template: JST["app/templates/article_list"],
-				collection: this
-			}));
-		});
+		template: JST["app/templates/article_list"]
 	}
 });
