7a14a501bc293034cef75aed30418a9ca7abfe5b
Amrut <amrut@amuratech.com>
2016-10-27 18:33:12 +0530

    presence true for text

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	validates :title, presence: {message: "Title field should bbe given"}, length: { minimum: 5 } 
+	validates :text, presence: true
 	puts ActiveModel::Validations::Callbacks.class	
 end
