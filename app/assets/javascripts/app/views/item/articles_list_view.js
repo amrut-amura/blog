b4c2a9789e6d376e29dd5681d63daba36d1eb294
Amrut <amrut@amuratech.com>
2016-11-02 16:06:28 +0530

    restful support for application

--- app/assets/javascripts/app/views/item/articles_list_view.js
+++ app/assets/javascripts/app/views/item/articles_list_view.js
@@ -7,21 +7,26 @@ App.Views.articleslistview = Marionette.CollectionView.extend({
 	childView: App.Views.articleitemview,
 	edit_article:function(event) {
 		event.preventDefault();
-		// debugger;
 		var article_id = $(event.currentTarget).data('article');
-		App.trigger("edit",article_id);
+		Backbone.history.navigate("articles/"+article_id+"/edit",true);
 	},
 	delete_article:function(event){
 		event.preventDefault();
 		var article_id = $(event.currentTarget).data('article');
 		var article_model = this.collection.get(article_id);
-		// debugger;
-		App.trigger("delete_article",article_model);
+		article_model.destroy().done({
+			success:function (model,response,options) {
+				Backbone.history.navigate("articles/",true);
+			},
+			error:function(model,response) {
+				console.log("error while deleting article");
+			}
+		});
 	},
 	show_article:function (event) {
 		event.preventDefault();
 		var article_id = $(event.currentTarget).data('article');
 		var article_model = this.collection.get(article_id);
-		App.trigger("show",article_model);
+		Backbone.history.navigate("articles/"+article_id,true);
 	}
 });
--- app/assets/javascripts/app/views/item/edit_article.js
+++ app/assets/javascripts/app/views/item/edit_article.js
@@ -7,7 +7,15 @@ App.Views.articleeditview = Marionette.ItemView.extend({
 		var title = $("#title").val();
 		var text = $("#text").val();
 		this.model.set({title:title,text:text});
-		// debugger;
-		App.trigger("update",this.model);
+		var article_model = this.model;
+		article_model.save({},{
+			success:function (model,options) {
+				 Backbone.history.navigate("/articles/"+model.id,true);
+				console.log("updated");
+			},
+			error:function (options) {
+				console.log("error while updating article");
+			}
+		})
 	}
 })
